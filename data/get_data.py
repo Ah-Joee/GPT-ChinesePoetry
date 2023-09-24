9c425def4530438f62196ff0ad13424f31256187
AhJoe <joseph3806@berkeley.edu>
2023-09-25 02:40:48 +0800

    modify get_data.py

--- data/get_data.py
+++ data/get_data.py
@@ -1,16 +1,21 @@
 # Only choose poetry with 5 words a sentence, 2 sentences in total
 
+# -*- conding:utf-8 -*-
 import glob
 import json
 
-datas = glob.glob("../TangPoetry/poet.*.json")
+datas = glob.glob("C:/Users/Josep/Desktop/GPT-Chinese/chinese-poetry-master/chinese-poetry-master/全唐诗/poet.*json")
 
 for data in datas:
+
+    # 載入文件
     with open(data, 'r', encoding='utf-8') as load:
         poetry = json.load(load)
+        # 取每一首詩的內容
         for p in poetry:
+            # 只取五言絕句
             content = p['paragraphs']
             if len(content)==2 and len(content[0])==12 and len(content[1])==12:
-                with open('../ChinesePoetry.txt', 'a', encoding='utf-8') as f:
-                    f.write("".join(content))
+                with open('./data/ChinesePoetry.txt', 'a', encoding='utf-8') as f:
+                    f.write("".join(p['paragraphs']))
                     f.write('\n')
