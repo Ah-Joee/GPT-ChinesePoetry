6fd857bfe56afd7a0a22af671cd7281f22fa4327
AhJoe <joseph3806@berkeley.edu>
2023-09-25 01:33:19 +0800

    model.py -- resolve config

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class LayerNorm(nn.Module):
 
 class CausalSelfAttention(nn.Module):
 
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
