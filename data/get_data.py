54abf4c5a8694e5f73cdce9a1c9950c61c4d0f9a
AhJoe <joseph3806@berkeley.edu>
2023-09-25 01:57:45 +0800

    get_data.py -- initial commit

---
+++ data/get_data.py
@@ -0,0 +1,17 @@
+# Only choose poetry with 5 words a sentence, 2 sentences in total
+
+import glob
+import json
+
+
+datas = glob.glob("../TangPoetry/poet.*.json")
+
+for data in datas:
+    with open(data, 'r', encoding='utf-8') as f:
+        poetry = json.load(f)
+        for p in poetry:
+            content = p['paragraphs']
+            if len(content)==2 and len(content[0])==12 and len(content[1])==12:
+                with open('poetry.txt', 'a', encoding='utf-8') as f:
+                    f.write("".join(content))
+                    f.write('\n')
