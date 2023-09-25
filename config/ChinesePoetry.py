c2cbf6abab8d32a86428e5c62b5b3d737f05389b
Joseph Lu <112120012+Ah-Joee@users.noreply.github.com>
2023-09-25 18:09:32 +0800

    Add files via upload

--- config/ChinesePoetry.py
+++ config/ChinesePoetry.py
@@ -12,17 +12,17 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'ChinesePoetry'
 gradient_accumulation_steps = 1
-batch_size = 64
+batch_size =24
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+n_layer = 8
+n_head = 8
+n_embd = 512
+dropout = 0.1
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 5e-4 # with baby networks can afford to go a bit higher
 max_iters = 5000
 lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
-beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+min_lr = 5e-5 # learning_rate / 10 usually
+beta2 = 0.97 # make a bit bigger because number of tokens per iter is small
